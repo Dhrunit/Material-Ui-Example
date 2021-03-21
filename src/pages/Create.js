4ad974c998e41edff7fcd9b40da79de8df9d60ef
Dhrunit <jimmyprajapati99@gmail.com>
2021-03-21 12:10:36 +0530

    Used the Typography component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 
 export default function Create() {
-  return (
-    <div>
-      Create page
-    </div>
-  )
+	return (
+		<div>
+			<Typography
+				variant='h6'
+				component='h2'
+				color='textSecondary'
+				align='center'>
+				Create a new Note
+			</Typography>
+			{/* <Typography color='error' noWrap>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+				Quibusdam saepe eius illo eum nisi facere ipsam, rerum quos
+				earum consequatur.
+			</Typography> */}
+		</div>
+	)
 }
