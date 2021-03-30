7db3bd04998ebdd621cd53056892f1e823af23ff
Dhrunit <jimmyprajapati99@gmail.com>
2021-03-30 14:01:44 +0530

    Used buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Container from '@material-ui/core/Container'
 
 export default function Create() {
 	return (
-		<div>
+		<Container size='sm'>
 			<Typography
 				variant='h6'
-				component='h2'
 				color='textSecondary'
-				align='center'>
-				Create a new Note
+				component='h2'
+				gutterBottom>
+				Create a New Note
 			</Typography>
-			{/* <Typography color='error' noWrap>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-				Quibusdam saepe eius illo eum nisi facere ipsam, rerum quos
-				earum consequatur.
-			</Typography> */}
-		</div>
+
+			<Button
+				onClick={() => console.log('you clicked me')}
+				type='submit'
+				color='secondary'
+				variant='contained'>
+				Submit
+			</Button>
+
+			<br />
+
+			<Button type='submit' color='secondary' variant='outlined'>
+				Submit
+			</Button>
+			<Button type='submit' color='default'>
+				Submit
+			</Button>
+
+			<ButtonGroup color='secondary' variant='contained' disableElevation>
+				<Button>One</Button>
+				<Button>Two</Button>
+				<Button>Three</Button>
+			</ButtonGroup>
+		</Container>
 	)
 }
