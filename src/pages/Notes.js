39d39d5a5d98c58554b6fc0466a53dbbe713d56b
Dhrunit <jimmyprajapati99@gmail.com>
2021-04-18 14:12:03 +0530

    Added React masonry css

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import Grid from '@material-ui/core/Grid'
-import { Container } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
+import Masonry from 'react-masonry-css'
 import NoteCard from '../components/NoteCard'
 
 export default function Notes() {
@@ -11,39 +11,33 @@ export default function Notes() {
 			.then((res) => res.json())
 			.then((data) => setNotes(data))
 	}, [])
+
 	const handleDelete = async (id) => {
 		await fetch('http://localhost:8000/notes/' + id, {
 			method: 'DELETE',
 		})
-		const newNotes = notes.filter((note) => note.id !== id)
+		const newNotes = notes.filter((note) => note.id != id)
 		setNotes(newNotes)
 	}
 
+	const breakpoints = {
+		default: 3,
+		1100: 2,
+		700: 1,
+	}
+
 	return (
-		<div>
-			{/* <Grid container>
-				<Grid item xs={12} md={3}>
-					<Paper>1</Paper>
-				</Grid>
-				<Grid item xs={12} md={3}>
-					<Paper>2</Paper>
-				</Grid>
-				<Grid item xs={12} md={3}>
-					<Paper>3</Paper>
-				</Grid>
-				<Grid item xs={12} md={3}>
-					<Paper>4</Paper>
-				</Grid>
-			</Grid> */}
-			<Container>
-				<Grid container spacing={3}>
-					{notes.map((note) => (
-						<Grid item key={note.id} xs={12} md={6} lg={4}>
-							<NoteCard note={note} handleDelete={handleDelete} />
-						</Grid>
-					))}
-				</Grid>
-			</Container>
-		</div>
+		<Container>
+			<Masonry
+				breakpointCols={breakpoints}
+				className='my-masonry-grid'
+				columnClassName='my-masonry-grid_column'>
+				{notes.map((note) => (
+					<div key={note.id}>
+						<NoteCard note={note} handleDelete={handleDelete} />
+					</div>
+				))}
+			</Masonry>
+		</Container>
 	)
 }
